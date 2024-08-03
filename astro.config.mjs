02602d73a5f7eb893f0b244800b49af2f5e60e70
Heraldo Fortuna <heraldofortuna@gmail.com>
2024-08-03 16:17:51 -0500

    se añadió tailwind y se creó el componente header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.heraldo-fortuna.com',
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
