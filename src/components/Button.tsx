70d74dcd90ce98d3019306ebfd4c03f3860af64a
Heraldo <heraldofortuna@gmail.com>
2024-08-04 20:03:25 -0500

    se hizo el efecto hover del componente botón

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,9 +5,10 @@ interface ButtonProps {
 
 const Button = ({ text, hasFullWidth = false }: ButtonProps) => {
   return (
-    <button className={`relative ${hasFullWidth ? 'w-full' : 'w-fit'} px-[32px] py-[16px] border border-dark-brown rounded-[8px] cursor-pointer`}>
-      <span className="block text-dark-brown">{text}</span>
-      <span className="absolute top-[6px] left-[6px] z-[-10] bg-yellow w-full h-full rounded-[8px]"></span>
+    <button className={`${hasFullWidth ? 'w-full' : 'w-fit'} relative px-[32px] py-[16px] rounded-[8px] cursor-pointer transform group`}>
+      <span className="absolute top-0 left-0 z-[10] w-full h-full border border-dark-brown rounded-[8px] transition-transform duration-300 transform group-hover:translate-x-[8px] group-hover:translate-y-[8px]"></span>
+      <span className="block z-[1] text-dark-brown transition-transform duration-300 transform group-hover:translate-x-[8px] group-hover:translate-y-[8px]">{text}</span>
+      <span className="absolute top-0 left-0 z-[-1] bg-yellow w-full h-full rounded-[8px] transform translate-x-[8px] translate-y-[8px]"></span>
     </button>
   )
 };
