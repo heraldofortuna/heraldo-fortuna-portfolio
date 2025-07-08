867e337068360ed82655b9366ea3122016c2f72c
Heraldo Fortuna <heraldofortuna@gmail.com>
2025-07-07 23:06:06 -0500

    se actualizó la información de trabajos y se mejoró el diseño de los proyectos

--- src/adapters/getGitHubRepos.ts
+++ src/adapters/getGitHubRepos.ts
@@ -3,16 +3,18 @@ import formatGitHubDate from "@utils/formatGitHubDate";
 
 async function getGitHubRepos(username: string): Promise<any> {
   const repos = await gitHubReposService(username);
-  const filteredRepos = repos.filter((repo: any) => repo.stargazers_count > 0);
-  const sortedRepos = filteredRepos.sort((a: any, b: any) => b.stargazers_count - a.stargazers_count);
+  const filteredRepos = repos.filter((repo: any) => repo.homepage && repo.homepage !== "");
+  const sortedRepos = filteredRepos.sort((repoA: any, repoB: any) => new Date(repoB.created_at).getTime() - new Date(repoA.created_at).getTime());
+  const repostList = filteredRepos.slice(0, 6); 
 
-  const formattedRepos = sortedRepos.map((repo: any) => ({
+  const formattedRepos = repostList.map((repo: any) => ({
     name: repo.name,
     description: repo.description || 'No description provided',
     date: formatGitHubDate(repo.created_at),
     image: `https://raw.githubusercontent.com/${username}/${repo.name}/main/public/screenshot.png`,
     stars: repo.stargazers_count,
-    url: repo.html_url,
+    repoUrl: repo.html_url,
+    demoUrl: repo.homepage,
   }));
 
   return formattedRepos;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -22,13 +22,13 @@ const Menu = () => {
   }, []);
 
   return (
-    <div className={`hidden md:block fixed right-0 z-10 top-[50%] -translate-y-1/2 bg-too-light-yellow text-dark-blue border border-r-0 border-dark-brown rounded-l-[8px] transition-transform duration-300 ${isVisible ? 'translate-x-0' : 'translate-x-full'}`}>
+    <div className={`hidden md:block fixed right-0 z-10 top-[50%] -translate-y-1/2 bg-light-yellow text-dark-blue border border-r-0 border-dark-brown rounded-l-[8px] transition-transform duration-300 ${isVisible ? 'translate-x-0' : 'translate-x-full'}`}>
       <nav>
         <ul>
           <li>
             <a
               href="#home"
-              className="block p-[16px] rounded-tl-[8px] transition-bg duration-300 hover:bg-light-yellow"
+              className="block p-[16px] rounded-tl-[8px] transition-bg duration-300 hover:bg-too-light-yellow"
             >
               <img src={homeIcon.src} width={32} height={32} alt="Home" />
             </a>
@@ -36,7 +36,7 @@ const Menu = () => {
           <li>
             <a
               href="#about"
-              className="block p-[16px] transition-bg duration-300 hover:bg-light-yellow"
+              className="block p-[16px] transition-bg duration-300 hover:bg-too-light-yellow"
             >
               <img src={aboutIcon.src} width={32} height={32} alt="About section menu option." />
             </a>
@@ -44,7 +44,7 @@ const Menu = () => {
           <li>
             <a
               href="#work"
-              className="block p-[16px] transition-bg duration-300 hover:bg-light-yellow"
+              className="block p-[16px] transition-bg duration-300 hover:bg-too-light-yellow"
             >
               <img src={workIcon.src} width={32} height={32} alt="Work section menu option." />
             </a>
@@ -52,7 +52,7 @@ const Menu = () => {
           <li>
             <a
               href="#contact"
-              className="block p-[16px] rounded-bl-[8px] transition-bg duration-300 hover:bg-light-yellow"
+              className="block p-[16px] rounded-bl-[8px] transition-bg duration-300 hover:bg-too-light-yellow"
             >
               <img src={contactIcon.src} width={32} height={32} alt="Contact section menu option." />
             </a>
